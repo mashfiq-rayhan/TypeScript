b55676bafdf6e7385f6c810bafd8b78bd9b55c11
mashfiq_rayhan <mashfiq.rayhan.ovi@gmail.com>
2024-10-07 15:07:46 +0600

    Day 2 Module 4

---
+++ 4. NextGenJSTS/src/app.ts
@@ -0,0 +1,82 @@
+console.log("Hello From TypeScript...");
+
+// const userName = "Mash";
+// let age = 30;
+// age = 29;
+
+// console.log(userName);
+// console.log(age);
+
+// function add(a:number, b:number) {
+//     let result = a + b;
+//     return result;
+// }
+
+// const add = (a:number, b:number) => {
+//     let res = a + b;
+//     return res;
+// }
+
+// const add = (a:number, b:number) => a + b;
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOut = (output: string|number) => {
+//     console.log(output);
+// }
+
+// const printOut = (output: string | number) => console.log(output);
+
+// const button = document.querySelector("button");
+
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
+
+// console.log(add(3, 5));
+// printOut(add(10,10));
+// printOut(add(10));
+
+// const hobbies = ['Sports', 'Coding'];
+
+// const actvHbs = ['Business'];
+
+// actvHbs.push(...hobbies);
+// hobbies.push(...actvHbs);
+
+// console.log(actvHbs);
+// console.log(hobbies);
+
+// const person = {
+//   name: "Mash",
+//   age: 30,
+// };
+
+// const cperson = { ...person };
+
+// console.log(person);
+// console.log(cperson);
+
+// const add = (...nums:number[]) => {
+//     return nums.reduce((res, val) => {
+//         return res * val;
+//     }, 1);
+// }
+
+// console.log(add(1,2,3,4,5,6,7,8,9));
+
+// const hobbies = ['Sports', 'Coding'];
+
+// const [h1, h2] = hobbies;
+
+// console.log(h1);
+// console.log(h2);
+
+// const person = {
+//   pname: "Mash",
+//   age: 30,
+// };
+
+// const { pname: aname, age} = person;
+// console.log(aname);
+// console.log(age);
