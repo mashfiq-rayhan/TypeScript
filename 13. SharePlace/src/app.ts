22a046b95e77fe1183c47f945cd3f4be6f282b9b
mashfiq_rayhan <mashfiq.rayhan.ovi@gmail.com>
2024-10-15 13:44:07 +0600

    typescript complete

---
+++ 13. SharePlace/src/app.ts
@@ -0,0 +1,12 @@
+// Code goes here!
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddress(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  
+}
+
+form.addEventListener("submit", searchAddress);
